2e287a63a18b4f26c0ff1d49c881906ed92afb26
DavidRMorphew <morphewd@umich.edu>
2021-03-19 10:19:36 -0400

    Done.

--- index.js
+++ index.js
@@ -9,5 +9,28 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+const greeting = document.getElementById('greeting');
+
 /* Write your implementation of greet() */
+function greet(timeString) {
+  const timeArray = timeString.split(":");
+  const parsedTimeArray = []
+  for (let i = 0; i < timeArray.length; i++) {
+    parsedTimeArray.push(parseInt(timeArray[i]));
+  }
+  const joinedArray = parsedTimeArray.join(".");
+  if (joinedArray < 12.00) {
+    return "Good Morning";
+  } else if (12.00 <= joinedArray && joinedArray <= 17.00) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  greeting.innerText = string;
+  return(string);
+}
+// console.log(displayMessage(greet("17:00")));
